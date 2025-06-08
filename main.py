993dda519100cca51709c8fe279cfb691be51cc6
Emil <emiljohansson0211@gmail.com>
2025-06-08 14:40:53 +0200

    Results final

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Find the effective ownership in focus company (CasaAS and ResightsApS) for each company in the ownership structure.
 
 # NOTES:
-# 1. All companies related to focus company are connected in a graph
+# 1. All companies related to focus company has a source-target relationship that will eventually lead to the focus company.
 #    Therefore, easier to start from the focus company and work our way backwards.
 # 2. Be aware that two companies can own shares in eachother.
 # 3. Example: A and B owns each 50% in C. C owns 50% in A. Effectively, 50% of A's ownership in C are shares C owns in itself and those shares will be distributed pro-rata to A and B.
@@ -12,12 +12,10 @@
 # 5. Some entries in the json files are inactive. They just add noise and will therefore be remove.
 # 6. If one company has two effective owerships of the focus company, the two effective ownerships can just be added together to get the total effective ownership.
 
-# %%
 import json
 import pydantic
 
 
-# %%
 class OwnershipNode(pydantic.BaseModel):
     id: str
     source: int
@@ -39,7 +37,6 @@ class Share(pydantic.BaseModel):
     upper_share: float
 
 
-# %%
 def fetch_data(target_company: str):
     with open(f"data/{target_company}.json", "r") as f:
         data = json.load(f)
@@ -64,6 +61,20 @@ def parse_share_interval(share_string: str):
     )
 
 
+def check_for_circular_ownership(
+    source_name: str, target_name: str, network: list[OwnershipNode]
+):
+    circular_ownership = False
+    for m in range(len(network)):
+        if (
+            network[m].source_name == target_name
+            and network[m].target_name == source_name
+            and network[m].active
+        ):
+            circular_ownership = True
+    return circular_ownership
+
+
 def calculate_circular_ownership(source_share: Share, target_share: Share):
     def calculator(source_share: float, target_share: float):
         direct_ownership = source_share * (1 - target_share)
@@ -82,6 +93,35 @@ def calculate_circular_ownership(source_share: Share, target_share: Share):
     return source_share
 
 
+def calculate_effective_ownership(
+    source_share: Share, current_node: OwnershipNode, previous_node: OwnershipNode
+):
+    # If real share is None, set it to 0. Otherwise we can't add to it.
+    # Which is necessary if a subsidiary down the line has two effective ownerships of the focus company.
+    current_node.real_lower_share = (
+        0 if current_node.real_lower_share is None else current_node.real_lower_share
+    )
+    current_node.real_average_share = (
+        0
+        if current_node.real_average_share is None
+        else current_node.real_average_share
+    )
+    current_node.real_upper_share = (
+        0 if current_node.real_upper_share is None else current_node.real_upper_share
+    )
+
+    current_node.real_lower_share += (
+        source_share.lower_share * previous_node.real_lower_share
+    )
+    current_node.real_average_share += (
+        source_share.average_share * previous_node.real_average_share
+    )
+    current_node.real_upper_share += (
+        source_share.upper_share * previous_node.real_upper_share
+    )
+    return current_node
+
+
 # Algorithm:
 # 1. If an entry has target_company
 # 2. -> Extract share interval from share string
@@ -94,13 +134,18 @@ def populate_effective_ownership(
     network: list[OwnershipNode],
     previous_node: OwnershipNode | None = None,
 ):
+    if previous_node:
+        previous_target_name = previous_node.target_name
+    else:
+        previous_target_name = ""
     result_network = network.copy()
     for n in range(len(network)):
+        # Check if this is a circular ownership to prevent infinity recursion.
         if (
-            network[n].target_name == target_company
-            and network[n].real_lower_share is None
-        ) and network[n].active:
-            # Extract numerical upper and lower share from share string and calculate average share.
+            previous_target_name != network[n].source_name
+            and network[n].target_name == target_company
+            and network[n].active
+        ):
             source_share = parse_share_interval(network[n].share)
             if previous_node:
                 # Check if a node exist with source_name equal to the current nodes target_name
@@ -115,31 +160,21 @@ def populate_effective_ownership(
                         source_share = calculate_circular_ownership(
                             source_share, target_share
                         )
-                result_network[n].real_lower_share = (
-                    source_share.lower_share * previous_node.real_lower_share
-                )
-                result_network[n].real_average_share = (
-                    source_share.average_share * previous_node.real_average_share
-                )
-                result_network[n].real_upper_share = (
-                    source_share.upper_share * previous_node.real_upper_share
+                result_network[n] = calculate_effective_ownership(
+                    source_share, network[n], previous_node
                 )
             else:
                 # No previous node means we are at the root of the tree
                 result_network[n].real_lower_share = source_share.lower_share
                 result_network[n].real_average_share = source_share.average_share
                 result_network[n].real_upper_share = source_share.upper_share
-            # Calculate average share
-            # Multiply the three different shares (lower, average, and upper) with the effective ownership from the current_node and add it the network.
-            # Save network entry into current_node
-            # Recursively call calculate_effective_ownership with the new current_node
+
             result_network = populate_effective_ownership(
-                network[n].source_name, result_network, network[n]
+                network[n].source_name, result_network, result_network[n]
             )
     return result_network
 
 
-# %%
 if __name__ == "__main__":
     for target_company in [
         {"comp_name": "CASA A/S", "file_name": "CasaAS"},
